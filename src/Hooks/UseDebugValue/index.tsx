002606f5b29f5c45196ff4dfcad00a0adc7c99c1
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-10-24 19:15:28 +0900

    fix: UseDebugValueのテスト実装完了

--- src/Hooks/UseDebugValue/index.tsx
+++ src/Hooks/UseDebugValue/index.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 
 const UseDebugValue = () => {
+  const useDelayedMessage = (msg: string, delay: number) => {
+    const [message, setMessage] = useState("");
+    useEffect(() => {
+      setTimeout(() => {
+        setMessage(msg);
+      }, delay);
+    });
+    React.useDebugValue(message ? "Message Set" : "Message not set");
+    return message;
+  };
+
     return (
       <>
         <h1>useDebugValueのテスト</h1>
         <Link to="/">Home</Link>
+        {useDelayedMessage("useDebugValueのテスト", 1500)}
+        <p>DevToolには、「DelayedMessage: useDebugValueのテスト」と表示される</p>
       </>
     )
 }
