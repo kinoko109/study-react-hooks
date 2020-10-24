d61856a2522804b9923cde4154de4cbca88ef7d8
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-10-24 18:42:42 +0900

    fix: useLayoutEffectのテスト実装完了

--- src/Hooks/UseLayoutEffect/index.tsx
+++ src/Hooks/UseLayoutEffect/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useLayoutEffect, useState} from "react";
 import {Link} from "react-router-dom";
 
 const UseLayoutEffect = () => {
+  const [count, setCount] = useState(0);
+
+  useLayoutEffect(() => {
+    console.log(count);
+  }, [])
+
+  useLayoutEffect(() => {
+    console.log(count);
+  }, [count])
+
     return (
       <>
         <h1>useLayoutEffectのテスト</h1>
         <Link to="/">Home</Link>
+        <br/>
+        <button onClick={() => setCount(count + 1)}>button</button>
+        <p>deps空のcount: {count} consoleには初回のみ表示</p>
+        <p>depsにcountを指定したcount: {count} consoleにはcountが変動と同時に表示</p>
       </>
     )
 }
