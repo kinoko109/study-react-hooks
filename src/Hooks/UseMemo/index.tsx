8d5914df404020073c4d7e5b807115b9d7eb4f0d
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-10-24 16:12:22 +0900

    fix: useMemoのテスト実装完了

--- src/Hooks/UseMemo/index.tsx
+++ src/Hooks/UseMemo/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useMemo, useState} from "react";
 import {Link} from "react-router-dom";
 
 const UseMemo = () => {
+  const [count, setCount] = useState(0);
+
+  const data1 = useMemo(() => {
+    return count * 100 / 2;
+  }, [])
+
+  const data2 = useMemo(() => {
+    return count * 100 / 2;
+  }, [count])
+
     return (
       <>
         <h1>useMemoのテスト</h1>
         <Link to="/">Home</Link>
+        <br />
+        <button onClick={() => setCount(count + 1)}>button</button>
+        <p>{data1}</p>
+        <p>{data2}</p>
       </>
     )
 }
